05657ee9965e4b070ea21e1f4209830e0ddd7fe4
Wilber30 <wilbersears@gmail.com>
2023-02-06 17:59:26 +0000

    Updates config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.php"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'Special': ["Shrikhand", 'serif']
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'antique': '#faebd7',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    extend: {
+      backgroundImage: {
+        'hero-banner': "url('/assets/images/My favourite.JPG')",
+      }
+    },
   },
   plugins: [],
 }
