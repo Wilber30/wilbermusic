3f5825357533269512e352a4d0cc5eacc208f8a4
Wilber30 <wilbersears@gmail.com>
2023-02-06 18:01:47 +0000

    Adds banner

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'templates/layout/ce_head.php' ?>
+
+<body>
+    <div class="relative">
+        <div class="h-screen bg-cover bg-bottom bg-hero-banner">
+            <div class="container m-auto">
+              <div class="flex">
+                <h1 class="font-Special mt-10 text-6xl text-antique drop-shadow-xl justify-start">Wilber Sears</h1>
+              </div>
+
+              <div class="container absolute bottom-6">
+                <div class="flex justify-center">
+                  <button class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-8 border border-gray-500 hover:border-transparent rounded">
+                    Listen Now 
+                  </button>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</body> 
