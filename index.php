f2f63f3e482242037a16d3db695ddeb93735c00d
Wilber30 <wilbersears@gmail.com>
2023-02-06 22:40:39 +0000

    Adds basic header from sm to lg breakpoint

--- index.php
+++ index.php
@@ -3,19 +3,25 @@
 <body>
     <div class="relative">
         <div class="h-screen bg-cover bg-bottom bg-hero-banner">
-            <div class="container m-auto">
-              <div class="flex">
-                <h1 class="font-Special mt-10 text-6xl text-antique drop-shadow-xl justify-start">Wilber Sears</h1>
-              </div>
+          <div class="bg-antique lg:hidden flex justify-center">
+              <p>Menu</p>
+          </div>
 
-              <div class="container absolute bottom-6">
-                <div class="flex justify-center">
-                  <button class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-8 border border-gray-500 hover:border-transparent rounded">
-                    Listen Now 
-                  </button>
-                </div>
+          <div class="w-full">
+            <div class="flex justify-center">
+              <h1 class=" mt-24 font-Special text-5xl lg:text-6xl text-biscuit drop-shadow-xl font-outline-2">
+                  Wilber Sears
+              </h1>
+            </div>
+
+            <div class="w-full absolute bottom-6">
+              <div class="flex justify-center">
+                <button class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-8 border border-gray-500 hover:border-transparent rounded">
+                  Listen Now 
+                </button>
               </div>
             </div>
+          </div>
         </div>
     </div>
 </body> 
