283186c8d8db8e8f70477031a6e759edd3fc120f
Wilber30 <wilbersears@gmail.com>
2023-02-07 15:03:26 +0000

    Adds header using include statement

--- index.php
+++ index.php
@@ -3,18 +3,16 @@
 <body>
     <div class="relative">
         <div class="h-screen bg-cover bg-bottom bg-hero-banner">
-          <div class="bg-antique lg:hidden flex justify-center">
-              <p>Menu</p>
-          </div>
+          <?php include 'templates/layout/header.php' ?>
 
-          <div class="w-full">
+          <div class="container mx-auto">
             <div class="flex justify-center">
-              <h1 class=" mt-24 font-Special text-5xl lg:text-6xl text-biscuit drop-shadow-xl font-outline-2">
+              <h1 id="banner-heading" class="mt-24 font-Special text-5xl lg:text-6xl text-biscuit drop-shadow-xl font-outline-2 animate-fade-in">
                   Wilber Sears
               </h1>
             </div>
 
-            <div class="w-full absolute bottom-6">
+            <div class="container mx-auto absolute bottom-6">
               <div class="flex justify-center">
                 <button class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-8 border border-gray-500 hover:border-transparent rounded">
                   Listen Now 
---
+++ templates/layout/header.php
@@ -0,0 +1,27 @@
+<div class="bg-antique opacity-90 md:hidden flex justify-end">
+    <button id="hamburger-btn" class="text-5xl cursor-pointer px-2 mb-1">
+        &#9776;
+    </button>
+    
+    <section id="mobile-menu" class="flex-col origin-top animate-open-menu hidden">
+        <button class="text-5xl px-2 mb-1 text-end">
+        &times;
+        </button>
+
+        <nav class="bg-antique opacity-90 top-12 right-0 w-screen">
+        <h2 class="text-2xl text-center font-Special font-outline-2 text-white">
+            Wilber Sears
+        </h2>
+
+        <ul class="text-center">
+            <li><a href="#" class="hover:opacity-80 inline-block">Home</a></li>
+
+            <li><a href="#" class="hover:opacity-80 inline-block">About Me</a></li>
+
+            <li><a href="#" class="hover:opacity-80 inline-block">Latest Videos</a></li>
+            
+            <li><a href="#" class="hover:opacity-80 inline-block">Contact</a></li>
+        </ul>
+        </nav>
+    </section>
+</div>
