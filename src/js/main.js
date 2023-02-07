bf9b06edfd93245fbef13b205d1b83ad9197541b
Wilber30 <wilbersears@gmail.com>
2023-02-07 19:04:26 +0000

    Adds toggleNav function

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const initApp = () => {
     const hamburgerBtn = document.getElementById('hamburger-btn')
+    const largeBurgerBtn = document.getElementById('lg-hamburger-btn')
     const mobileMenu = document.getElementById('mobile-menu')
+    const sideNav = document.getElementById('side-nav')
     const bannerHeading =document.getElementById('banner-heading')
 
     const toggleMenu = () => {
@@ -12,8 +14,17 @@ const initApp = () => {
         bannerHeading.classList.toggle('hidden')
     }
 
+    const toggleNav = () => {
+        sideNav.classList.toggle('hidden')
+        sideNav.classList.toggle('flex')
+        largeBurgerBtn.classList.toggle('flex')
+        largeBurgerBtn.classList.toggle('hidden')
+    }
+
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
+    largeBurgerBtn.addEventListener('click', toggleNav)
+    sideNav.addEventListener('click', toggleNav)
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
