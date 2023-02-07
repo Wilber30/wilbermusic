f0d97b0289b38508bc4aa64bf16c6b9b9b168934
Wilber30 <wilbersears@gmail.com>
2023-02-07 19:04:48 +0000

    Adds side navigation

---
+++ templates/layout/side_navigation.php
@@ -0,0 +1,23 @@
+<div id="side" class="hidden md:flex justify-end">
+  <button id="lg-hamburger-btn" class="text-7xl text-grey cursor-pointer px-2 mb-1">
+    &#9776;
+  </button>
+    
+  <section id="side-nav" class="flex-col origin-top hidden">
+    <button class="text-7xl text-grey px-2 mb-1 text-end animate-large-open-menu-icon">
+        &times;
+    </button>
+
+    <nav class="bg-antique opacity-90 absolute z-50 top-6 right-4 h-2/5 w-1/3 animate-large-open-menu">
+      <ul class="text-center">
+        <li><a href="#" class="hover:opacity-80 inline-block">Home</a></li>
+
+        <li><a href="#" class="hover:opacity-80 inline-block">About Me</a></li>
+
+        <li><a href="#" class="hover:opacity-80 inline-block">Latest Videos</a></li>
+        
+        <li><a href="#" class="hover:opacity-80 inline-block">Contact</a></li>
+      </ul>
+    </nav>
+  </section>
+</div>
