0eef9abc507f081d3ff7544e904f084f2c396803
Wilber30 <wilbersears@gmail.com>
2023-02-08 14:34:11 +0000

    Adds button and classes

--- templates/layout/header.php
+++ templates/layout/header.php
@@ -1,27 +1,49 @@
 <div class="bg-antique opacity-90 md:hidden flex justify-end">
-    <button id="hamburger-btn" class="text-5xl cursor-pointer px-2 mb-1">
-        &#9776;
-    </button>
-    
-    <section id="mobile-menu" class="flex-col origin-top animate-small-open-menu hidden">
-        <button class="text-5xl px-2 mb-1 text-end">
-        &times;
-        </button>
+   <section>
+        <div class="flex justify-end">
+            <button id="hamburger-button" class="relative w-16 h-14 cursor-pointer z-10">
+                <div class="bg-dark-grey w-8 h-1 rounded absolute top-7 -mt-0.5 
+                transition-all duration-500
+                before:content-[''] before:bg-dark-grey 
+                before:w-8 before:h-1
+                before:rounded before:absolute 
+                before:-translate-x-4
+                before:-translate-y-3
+                before:transition-all before:duration-500
+                after:content-[''] after:bg-dark-grey 
+                after:w-8 after:h-1
+                after:rounded after:absolute 
+                after:-translate-x-4
+                after:translate-y-3
+                after:transition-all after:duration-500
+                "></div>
+            </button>
+        </div>
+        
+        <div id="mobile-menu" class="origin-top animate-small-open-menu hidden">
+            <nav class="bg-antique opacity-90 top-12 right-0 w-screen">
+                <h2 class="text-2xl text-center font-Special font-outline-2 text-white">
+                    Wilber Sears
+                </h2>
 
-        <nav class="bg-antique opacity-90 top-12 right-0 w-screen">
-        <h2 class="text-2xl text-center font-Special font-outline-2 text-white">
-            Wilber Sears
-        </h2>
+                <ul class="text-center mb-4">
+                    <li><a href="#" class="hover:opacity-80 inline-block">Home</a></li>
 
-        <ul class="text-center">
-            <li><a href="#" class="hover:opacity-80 inline-block">Home</a></li>
+                    <li><a href="#" class="hover:opacity-80 inline-block">About Me</a></li>
 
-            <li><a href="#" class="hover:opacity-80 inline-block">About Me</a></li>
+                    <li><a href="#" class="hover:opacity-80 inline-block">Latest Videos</a></li>
+                    
+                    <li><a href="#" class="hover:opacity-80 inline-block">Contact</a></li>
+                </ul>
 
-            <li><a href="#" class="hover:opacity-80 inline-block">Latest Videos</a></li>
-            
-            <li><a href="#" class="hover:opacity-80 inline-block">Contact</a></li>
-        </ul>
-        </nav>
+                <div class="flex justify-center gap-5 mb-4">
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </nav>
+        </div>
     </section>
 </div>
