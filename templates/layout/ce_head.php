1a9d6653d39fa8611a8ab2312a2ee2559555c2d1
Wilber30 <wilbersears@gmail.com>
2023-02-10 15:19:57 +0000

    Adds link to google font

--- templates/layout/ce_head.php
+++ templates/layout/ce_head.php
@@ -14,5 +14,6 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Shrikhand&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700&family=Shrikhand&display=swap" rel="stylesheet">
   <script src="/src/js/main.js" defer></script>
 </head>
