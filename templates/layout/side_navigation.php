c4daf7e060a395b022bad42a2d963a6283ed7007
Wilber30 <wilbersears@gmail.com>
2023-02-10 15:20:50 +0000

    Updates mark-up and classes

--- templates/layout/side_navigation.php
+++ templates/layout/side_navigation.php
@@ -1,23 +1,55 @@
-<div id="side" class="hidden md:flex justify-end">
-  <button id="lg-hamburger-btn" class="text-7xl text-grey cursor-pointer px-2 mb-1">
-    &#9776;
-  </button>
+
+  
+  <div class="hidden md:flex h-screen fixed justify-center w-72 right-0">
+
+    <div class="flex justify-end mt-10">
+      <button id="large-hamburger-button" class="relative w-16 h-14 cursor-pointer z-10 open">
+          <div class="bg-white w-14 h-1 rounded -mt-0.5 
+            transition-all duration-500
+            before:content-[''] before:bg-white
+            before:w-14 before:h-1
+            before:rounded before:absolute 
+            before:-translate-x-7
+            before:-translate-y-3
+            before:transition-all before:duration-500
+            after:content-[''] after:bg-white
+            after:w-14 after:h-1
+            after:rounded after:absolute 
+            after:-translate-x-7
+            after:translate-y-3
+            after:transition-all after:duration-500">
+          </div>
+        </button>
+    </div>
     
-  <section id="side-nav" class="flex-col origin-top hidden">
-    <button class="text-7xl text-grey px-2 mb-1 text-end animate-large-open-menu-icon">
-        &times;
-    </button>
+    <div id="side-nav" class="hidden justify-center bg-antique pt-10 opacity-90 w-11/12 animate-large-menu-visible">
+      <nav class="flex justify-evenly flex-col">
+        <div>
+          <ul class="text-center">
 
-    <nav class="bg-antique opacity-90 absolute z-50 top-6 right-4 h-2/5 w-1/3 animate-large-open-menu">
-      <ul class="text-center">
-        <li><a href="#" class="hover:opacity-80 inline-block">Home</a></li>
+            <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Home</a></li>
 
-        <li><a href="#" class="hover:opacity-80 inline-block">About Me</a></li>
+            <li><a href="#" class="mb-10 hover:opacity-80 inline-block">About Me</a></li>
 
-        <li><a href="#" class="hover:opacity-80 inline-block">Latest Videos</a></li>
-        
-        <li><a href="#" class="hover:opacity-80 inline-block">Contact</a></li>
-      </ul>
+            <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Latest Videos</a></li>
+            
+            <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Contact</a></li>
+
+          </ul>
+
+          <div class="flex justify-center gap-5 text-2xl my-4">
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+          </div>
+        </div>
+
+
+        <p class="text-center">&#169;Bellybones Records <br> 2023</p>
     </nav>
-  </section>
+  </div>
 </div>
+   
+ 
