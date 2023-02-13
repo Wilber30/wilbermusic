77016018fdab6e8297b1630badaff6c7300e73fb
Wilber30 <wilbersears@gmail.com>
2023-02-13 17:24:58 +0000

    Adds side nav component and include

---
+++ templates/components/side-nav-button.php
@@ -0,0 +1,19 @@
+<div class="flex justify-end mt-10">
+  <button id="lg-hamburger-button" class="relative w-16 h-14 cursor-pointer z-10 open">
+    <div class="bg-white w-14 h-1 rounded -mt-0.5 
+      transition-all duration-500
+      before:content-[''] before:bg-white
+      before:w-14 before:h-1
+      before:rounded before:absolute 
+      before:-translate-x-7
+      before:-translate-y-3
+      before:transition-all before:duration-500
+      after:content-[''] after:bg-white
+      after:w-14 after:h-1
+      after:rounded after:absolute 
+      after:-translate-x-7
+      after:translate-y-3
+      after:transition-all after:duration-500">
+    </div>
+  </button>
+</div>
--- templates/layout/side_navigation.php
+++ templates/layout/side_navigation.php
@@ -1,53 +1,29 @@
+<div class="hidden lg:flex h-screen fixed justify-center w-72 xl:w-96 right-0">
+    <?php include './templates/components/side-nav-button.php' ?>
+  <div id="side-nav" class="flex justify-center bg-antique pt-10 opacity-90 w-11/12 xl:w-full translate-x-full transition-all ease-in-out duration-500">
+    <nav class="flex justify-evenly flex-col">
+      <div>
+        <ul class="text-center">
 
-  
-  <div class="hidden lg:flex h-screen fixed justify-center w-72 xl:w-96 right-0">
+          <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Home</a></li>
 
-    <div class="flex justify-end mt-10">
-      <button id="large-hamburger-button" class="relative w-16 h-14 cursor-pointer z-10 open">
-          <div class="bg-white w-14 h-1 rounded -mt-0.5 
-            transition-all duration-500
-            before:content-[''] before:bg-white
-            before:w-14 before:h-1
-            before:rounded before:absolute 
-            before:-translate-x-7
-            before:-translate-y-3
-            before:transition-all before:duration-500
-            after:content-[''] after:bg-white
-            after:w-14 after:h-1
-            after:rounded after:absolute 
-            after:-translate-x-7
-            after:translate-y-3
-            after:transition-all after:duration-500">
-          </div>
-        </button>
-    </div>
-    
-    <div id="side-nav" class="flex justify-center bg-antique pt-10 opacity-90 w-11/12 xl:w-full translate-x-full transition-all ease-in-out duration-500">
-      <nav class="flex justify-evenly flex-col">
-        <div>
-          <ul class="text-center">
+          <li><a href="#" class="mb-10 hover:opacity-80 inline-block">About Me</a></li>
 
-            <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Home</a></li>
+          <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Latest Videos</a></li>
+          
+          <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Contact</a></li>
 
-            <li><a href="#" class="mb-10 hover:opacity-80 inline-block">About Me</a></li>
+        </ul>
 
-            <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Latest Videos</a></li>
-            
-            <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Contact</a></li>
+        <div class="flex justify-center gap-5 text-2xl my-4">
+          <a href="#"><i class="fa-brands fa-instagram"></i></a>
 
-          </ul>
+          <a href="#"><i class="fa-brands fa-facebook"></i></a>
 
-          <div class="flex justify-center gap-5 text-2xl my-4">
-            <a href="#"><i class="fa-brands fa-instagram"></i></a>
-
-            <a href="#"><i class="fa-brands fa-facebook"></i></a>
-
-            <a href="#"><i class="fa-brands fa-youtube"></i></a>
-          </div>
+          <a href="#"><i class="fa-brands fa-youtube"></i></a>
         </div>
-
-
-        <p class="text-center">&#169;Bellybones Records <br> 2023</p>
+      </div>
+      <p class="text-center">&#169;Bellybones Records <br> 2023</p>
     </nav>
   </div>
 </div>
