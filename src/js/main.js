2c7220a13eb9858f0899a6342715917539e18896
Wilber30 <wilbersears@gmail.com>
2023-02-13 17:24:17 +0000

    Organises code

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,34 @@
 const initApp = () => {
-    const hamburgerBtn = document.getElementById('hamburger-button')
-    const largeBurgerBtn = document.getElementById('large-hamburger-button')
+    
+    // Header variables
+    const smallHamburgerBtn = document.getElementById('sm-hamburger-button')
     const mobileMenu = document.getElementById('mobile-menu')
-    const sideNav = document.getElementById('side-nav')
     const bannerHeading = document.getElementById('banner-heading')
     const smallHeaderButton = document.getElementById('sm-header-button')
-    const smallNavSection = document.getElementById('small-nav-whole')
+    const smallNavSection = document.getElementById('sm-nav-whole')
 
+    // Side-nav variables
+    const largeBurgerBtn = document.getElementById('lg-hamburger-button')
+    const sideNav = document.getElementById('side-nav')
 
     const toggleMenu = () => {
-        hamburgerBtn.classList.toggle('toggle-btn')
-        bannerHeading.classList.toggle('flex')
-        bannerHeading.classList.toggle('hidden')
-        mobileMenu.classList.toggle('toggle-header')
-        smallHeaderButton.classList.toggle('bg-antique')
-        smallNavSection.classList.toggle('bg-antique')
+			smallHamburgerBtn.classList.toggle('toggle-btn')
+			bannerHeading.classList.toggle('flex')
+			bannerHeading.classList.toggle('hidden')
+			mobileMenu.classList.toggle('toggle-header')
+			smallHeaderButton.classList.toggle('bg-antique')
+			smallNavSection.classList.toggle('bg-antique')
     }
 
     const toggleNav = () => {
-        largeBurgerBtn.classList.toggle('toggle-btn')
-        sideNav.classList.toggle('toggle-nav')
-
+			largeBurgerBtn.classList.toggle('toggle-btn')
+			sideNav.classList.toggle('toggle-nav')
     }
 
-    hamburgerBtn.addEventListener('click', toggleMenu)
+    smallHamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
     largeBurgerBtn.addEventListener('click', toggleNav)
     sideNav.addEventListener('click', toggleNav)
-
 
 }
 
