208d3fbf1faea0132ec17243d60a08aabc522027
Wilber30 <wilbersears@gmail.com>
2023-02-13 17:25:14 +0000

    Updates classes

--- templates/layout/header.php
+++ templates/layout/header.php
@@ -1,32 +1,34 @@
-<div class= "opacity-90 lg:hidden flex justify-end absolute z-50">
-    <section>
-        <div id="small-nav-whole" class="origin-top relative">
-            <?php include './templates/components/header_button.php' ?>
-                <nav id="mobile-menu" class="flex flex-col justify-evenly gap-4 text-center bg-antique opacity-90 w-screen -translate-y-[120%] transition-all ease-in-out duration-500">
-                <h2 class="text-4xl font-Special font-outline-2 text-white">
-                    Wilber Sears
-                </h2>
+<div class= "opacity-90 h-[60%] lg:hidden flex justify-end absolute z-50">
+  <section id="sm-nav-whole" class="relative">
+      <?php include './templates/components/header_button.php' ?>
+    <div class="origin-top relative">
+      <nav id="mobile-menu" class="w-screen flex flex-col justify-evenly gap-4 text-center bg-antique 
+          -translate-y-[120%] transition-all ease-in-out duration-500">
+        <h2 class="text-4xl font-Special font-outline-2 text-white">
+            Wilber Sears
+        </h2>
 
-                <ul class="mb-4">
-                    <li><a href="#" class="mb-4 hover:opacity-80 inline-block">Home</a></li>
+        <ul class="mb-4">
+            <li><a href="#" class="mb-4 hover:opacity-80 inline-block">Home</a></li>
 
-                    <li><a href="#" class="mb-4 hover:opacity-80 inline-block">About Me</a></li>
+            <li><a href="#" class="mb-4 hover:opacity-80 inline-block">About Me</a></li>
 
-                    <li><a href="#" class="mb-4 hover:opacity-80 inline-block">Latest Videos</a></li>
-                    
-                    <li><a href="#" class="mb-4 hover:opacity-80 inline-block">Contact</a></li>
-                </ul>
+            <li><a href="#" class="mb-4 hover:opacity-80 inline-block">Latest Videos</a></li>
+            
+            <li><a href="#" class="mb-4 hover:opacity-80 inline-block">Contact</a></li>
+        </ul>
 
-                <div class="flex justify-center gap-5 text-xl mb-4">
-                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        <div class="flex justify-center gap-5 text-xl mb-4">
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
 
-                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
 
-                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                </div>
-
-                <p class="mb-6">&#169;Bellybones Records 2023</p>
-            </nav>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
         </div>
-    </section>
+
+        <p class="mb-6">&#169;Bellybones Records 2023</p>
+
+      </nav>
+    </div>
+  </section>
 </div> 
