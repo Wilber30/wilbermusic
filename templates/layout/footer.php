51c9cc02be355d21bca7a4b9e2779db85da236b8
Wilber30 <wilbersears@gmail.com>
2023-02-21 17:11:39 +0000

    Adds footer

---
+++ templates/layout/footer.php
@@ -0,0 +1,18 @@
+<footer class="bg-khaki py-5">
+  <div class="container mx-auto text-center text-white">
+    <div class="flex justify-center gap-5 text-4xl mb-4">
+      <a href="#"><i class="fa-brands fa-instagram"></i></a>
+
+      <a href="#"><i class="fa-brands fa-facebook"></i></a>
+
+      <a href="#"><i class="fa-brands fa-youtube"></i></a>
+    </div>
+
+
+    <div>
+      <p class="">
+        &#169;Bellybones Records 2023
+      </p>
+    </div>
+  </div>
+</footer>
