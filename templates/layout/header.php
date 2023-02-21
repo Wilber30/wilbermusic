6ce5433f8246a628e13ecdfc437be6c486ff9a7d
Wilber30 <wilbersears@gmail.com>
2023-02-21 17:12:32 +0000

    Adds id

--- templates/layout/header.php
+++ templates/layout/header.php
@@ -1,9 +1,10 @@
-<div class= "opacity-90 h-[60%] lg:hidden flex justify-end absolute z-50">
+<div id="header-section" class="header-section opacity-90 h-[60%] lg:hidden flex justify-end absolute z-50">
   <section id="sm-nav-whole" class="relative">
       <?php include './templates/components/header_button.php' ?>
     <div class="origin-top relative">
-      <nav id="mobile-menu" class="w-screen flex flex-col justify-evenly gap-4 text-center bg-antique 
+      <nav id="mobile-menu" class="w-screen fixed flex flex-col justify-evenly gap-4 text-center bg-antique 
           -translate-y-[120%] transition-all ease-in-out duration-500">
+
         <h2 class="text-4xl font-Special font-outline-2 text-white">
             Wilber Sears
         </h2>
