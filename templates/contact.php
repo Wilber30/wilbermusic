c1f73ec8766decb2868c8c0b2b8a5f20cd8b0925
Wilber30 <wilbersears@gmail.com>
2023-03-27 15:30:32 +0100

    Adds contact component

---
+++ templates/contact.php
@@ -0,0 +1,17 @@
+<?php include 'layout/contact_head.php' ?>
+
+<body id="body" class="w-screen">
+  <div class="full relative"></div>
+    <div id="cover" class="absolute h-full z-30 hidden bg-black/50 w-screen"></div>
+      <div class="relative">
+        <?php include 'layout/header.php' ?> 
+
+        <?php include 'layout/side_navigation.php' ?> 
+        <div class="container mx-auto">
+          <?php include 'components/contact.php' ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+ 
