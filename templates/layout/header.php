bdc3d1b6051cd2ccee6eaae3f6520fa31f211383
Wilber30 <wilbersears@gmail.com>
2023-03-27 15:31:21 +0100

    Adds buttons

--- templates/layout/header.php
+++ templates/layout/header.php
@@ -1,6 +1,24 @@
 <div id="header-section" class="header-section opacity-90 h-[60%] lg:hidden flex justify-end absolute z-50">
   <section id="sm-nav-whole" class="relative">
-      <?php include './templates/components/header_button.php' ?>
+    <div id="sm-header-button" class="flex bg-antique justify-end fixed w-full z-50">
+      <button id="sm-hamburger-button" class="relative w-16 h-14 cursor-pointer">
+          <div class="bg-dark-grey w-8 h-1 rounded absolute top-7 -mt-0.5 
+          transition-all duration-500
+          before:content-[''] before:bg-dark-grey 
+          before:w-8 before:h-1
+          before:rounded before:absolute 
+          before:-translate-x-4
+          before:-translate-y-3
+          before:transition-all before:duration-500
+          after:content-[''] after:bg-dark-grey 
+          after:w-8 after:h-1
+          after:rounded after:absolute 
+          after:-translate-x-4
+          after:translate-y-3
+          after:transition-all after:duration-500
+          "></div>
+      </button>
+    </div>  
     <div class="origin-top relative">
       <nav id="mobile-menu" class="w-screen fixed flex flex-col justify-evenly gap-4 text-center bg-antique 
           -translate-y-[120%] transition-all ease-in-out duration-500">
--- templates/layout/side_navigation.php
+++ templates/layout/side_navigation.php
@@ -1,5 +1,23 @@
 <div id="whole-nav-section" class="hidden lg:flex h-screen absolute z-50 justify-center w-72 xl:w-96 right-[-50px]">
-    <?php include './templates/components/side-nav-button.php' ?>
+  <div class="flex justify-end mt-10">
+    <button id="lg-hamburger-button" class="relative w-16 h-14 cursor-pointer z-10 open">
+      <div class="bg-white w-14 h-1 rounded -mt-0.5 
+        transition-all duration-500
+        before:content-[''] before:bg-white
+        before:w-14 before:h-1
+        before:rounded before:absolute 
+        before:-translate-x-7
+        before:-translate-y-3
+        before:transition-all before:duration-500
+        after:content-[''] after:bg-white
+        after:w-14 after:h-1
+        after:rounded after:absolute 
+        after:-translate-x-7
+        after:translate-y-3
+        after:transition-all after:duration-500">
+      </div>
+    </button>
+  </div>
   <div id="side-nav" class="flex justify-center bg-antique pt-10 opacity-90 w-11/12 xl:w-full translate-x-full transition-all ease-in-out duration-500">
     <nav class="flex justify-evenly flex-col">
       <div>
@@ -11,7 +29,7 @@
 
           <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Latest Videos</a></li>
           
-          <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Contact</a></li>
+          <li><a href="templates/contact.php" class="mb-10 hover:opacity-80 inline-block">Contact</a></li>
 
         </ul>
 
