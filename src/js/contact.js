2db19af8619114703c36fe0f78624ac73e71daeb
Wilber30 <wilbersears@gmail.com>
2023-03-28 17:52:05 +0100

    Updates variables and functions

--- src/js/contact.js
+++ src/js/contact.js
@@ -14,17 +14,15 @@ const initApp = () => {
 
     const toggleMenu = () => {
     smallHamburgerBtn.classList.toggle('toggle-btn')
-	mobileMenu.classList.toggle('toggle-header')
+	  mobileMenu.classList.toggle('toggle-header')
     mobileMenu.classList.toggle('is-active')
-	smallHeaderButton.classList.add('bg-antique')
-    smallHeaderButton.classList.toggle("border-b-4", "border-dark-grey")
-    smallHeaderButton.classList.toggle("border-none")
+	  smallHeaderButton.classList.add('bg-antique')
     overlay.classList.toggle('!flex')
     }
 
     const toggleNav = () => {
-	largeBurgerBtn.classList.toggle('toggle-btn')
-	sideNav.classList.toggle('toggle-nav')
+	  largeBurgerBtn.classList.toggle('toggle-btn')
+	  sideNav.classList.toggle('toggle-nav')
     wholeNavSection.classList.toggle('!right-0')
     overlay.classList.toggle('!flex')
     body.classList.toggle('fixed')
@@ -39,23 +37,5 @@ const initApp = () => {
 
 document.addEventListener('DOMContentLoaded', initApp)
 
-const header = document.getElementById('header-section') 
-const smallHeaderButton = document.getElementById('sm-header-button')
-const mobileMenu = document.getElementById('mobile-menu')
 
-window.addEventListener('scroll', () => {
-  let scrollPosition = window.scrollY + 80;
-
-  if(scrollPosition >= 500) {
-    header.classList.add("opacity-100")
-    header.classList.remove("opacity-90")
-    smallHeaderButton.classList.add("border-b-4", "border-dark-grey")
-    mobileMenu.classList.add("border-b-4", "border-dark-grey")
-  } else if(scrollPosition <= 500) {
-    header.classList.add('opacity-90')
-    header.classList.remove("opacity-100")
-    smallHeaderButton.classList.remove("border-b-4", "border-dark-grey")
-    mobileMenu.classList.remove("border-b-4", "border-dark-grey")
-  } 
-})
 
