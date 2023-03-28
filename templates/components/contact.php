42f9bac925ffca9c2e30c8e5cd9b0aa5c97a1899
Wilber30 <wilbersears@gmail.com>
2023-03-28 17:52:19 +0100

    Adds classes

--- templates/components/contact.php
+++ templates/components/contact.php
@@ -1,20 +1,21 @@
-<div class="font-Line">
-  <form class="flex flex-col" action="post">
+<div class="flex flex-row justify-center font-Line">
+  <form class="flex flex-col w-4/5" action="post">
     <div id="response"></div>
 
     <div id="error"></div>
 
-    <div class="flex">
-      <input class="w-1/2" id="name" type="text" name="name" placeholder="Name:" required="required">
+    <div class="border-4 border-solid border-grey p-3 rounded-md bg-whitesmoke">
+      <div class="flex !text-black">
+        <input class="w-1/2 bg-whitesmoke" id="name" type="text" name="name" placeholder="Name:" required="required">
 
-      <input class="w-1/2" id="email" name="email" type="text" placeholder="Email:" required="required">
+        <input class="w-1/2 bg-whitesmoke" id="email" name="email" type="text" placeholder="Email:" required="required">
+      </div>
+      <input id="subject" class="w-full mt-2 bg-whitesmoke" type="text" name="subject" placeholder="Subject" required="required">
+    
+      <textarea id="message" class="w-full mt-2 bg-whitesmoke" name="message" rows="8" cols="80" placeholder="Message:" required></textarea>
     </div>
-    <input id="subject" type="text" name="subject" placeholder="Subject" required="required">
-
-    <textarea id="message" name="message" rows="8" cols="80" placeholder="Message:" required></textarea>
-
-    <div>
-      <button id="sub_btn" type="submit" name="Submit">Submit</button>
+    <div class="text-center">
+      <button class="my-4" id="sub_btn" type="submit" name="Submit">Submit</button>
     </div>
   </form>
 </div>
