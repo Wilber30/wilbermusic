38fdcc76ec1c8eaca0fe4d7c8b2e4f30a43d84c7
Wilber30 <wilbersears@gmail.com>
2023-03-28 17:52:35 +0100

    Updates mark-up and classes

--- templates/contact.php
+++ templates/contact.php
@@ -1,16 +1,26 @@
 <?php include 'layout/contact_head.php' ?>
 
-<body id="body" class="w-screen">
-  <div class="full relative"></div>
+<body id="body" class="w-screen overflow-x-hidden">
+  <div class="h-screen">
     <div id="cover" class="absolute h-full z-30 hidden bg-black/50 w-screen"></div>
-      <div class="relative">
-        <?php include 'layout/header.php' ?> 
+      <div class="flex flex-col justify-between h-screen bg-white">
+        <div class="[&_.button-inner]:before:bg-black [&_.button-inner]:bg-black [&_.button-inner]:after:bg-black">
+          <?php include 'layout/header.php' ?> 
 
-        <?php include 'layout/side_navigation.php' ?> 
-        <div class="container mx-auto">
-          <?php include 'components/contact.php' ?>
+          <?php include 'layout/side_navigation.php' ?> 
         </div>
-      </div>
+        <div class="container mx-auto">
+          <div class="mt-8 flex flex-col justify-evenly text-center">
+            <h1 class="my-6 text-center font-Special text-5xl text-white drop-shadow-xl font-outline-2">
+                Contact
+            </h1>
+
+            <p class="mb-8">Please feel free to contact me using the form below</p>
+
+            <?php include 'components/contact.php' ?>
+          </div>
+        </div>
+      <?php include 'layout/footer.php' ?>
     </div>
   </div>
 </body>
