66dfddc52479604a31af56c414617b5bfa6c896d
Wilber30 <wilbersears@gmail.com>
2023-03-28 17:53:16 +0100

    Adds class

--- templates/layout/side_navigation.php
+++ templates/layout/side_navigation.php
@@ -1,7 +1,7 @@
 <div id="whole-nav-section" class="hidden lg:flex h-screen absolute z-50 justify-center w-72 xl:w-96 right-[-50px]">
   <div class="flex justify-end mt-10">
     <button id="lg-hamburger-button" class="relative w-16 h-14 cursor-pointer z-10 open">
-      <div class="bg-white w-14 h-1 rounded -mt-0.5 
+      <div id="btn-inner" class="bg-white w-14 h-1 rounded -mt-0.5 button-inner
         transition-all duration-500
         before:content-[''] before:bg-white
         before:w-14 before:h-1
@@ -23,7 +23,7 @@
       <div>
         <ul class="text-center">
 
-          <li><a href="#" class="mb-10 hover:opacity-80 inline-block">Home</a></li>
+          <li><a href="/index.php" class="mb-10 hover:opacity-80 inline-block">Home</a></li>
 
           <li><a href="#" class="mb-10 hover:opacity-80 inline-block">About Me</a></li>
 
