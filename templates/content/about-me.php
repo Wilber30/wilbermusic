144a0365d944ff9bd05e9a42b10d5a9ff21b4e21
Wilber30 <wilbersears@gmail.com>
2023-03-30 20:59:31 +0100

    Adds about-me content

---
+++ templates/content/about-me.php
@@ -0,0 +1,19 @@
+<div>
+	<h2 class="my-4">Who am I?</h2>
+		<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+				Sollicitudin nibh sit amet commodo. Risus in hendrerit gravida rutrum quisque. Nunc mattis enim ut tellus elementum sagittis vitae. 
+				Eget felis eget nunc lobortis. Tellus molestie nunc non blandit massa enim. Est lorem ipsum dolor sit amet. Nibh tellus molestie nunc non blandit massa enim nec dui. 
+				Tristique senectus et netus et malesuada. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. 
+				Eu lobortis elementum nibh tellus molestie nunc. Tempor commodo ullamcorper a lacus vestibulum sed arcu non odio.
+		</p><br>
+
+		<p>
+				Volutpat consequat mauris nunc congue nisi. Amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus. 
+				Morbi tincidunt ornare massa eget egestas purus viverra accumsan. Accumsan lacus vel facilisis volutpat est velit egestas. Duis ut diam quam nulla porttitor. 
+				Est velit egestas dui id ornare arcu odio ut sem. Scelerisque felis imperdiet proin fermentum leo vel orci. A diam maecenas sed enim ut sem. 
+				Nunc sed id semper risus in hendrerit gravida rutrum. Malesuada bibendum arcu vitae elementum. Aenean euismod elementum nisi quis eleifend quam adipiscing. 
+				Aliquet nec ullamcorper sit amet risus nullam. Quam pellentesque nec nam aliquam sem et tortor consequat.
+		</p>
+</div>
+	
