e2f18e4d0acdfa9aa53e90a2b0c3faf0de57f503
Wilber30 <wilbersears@gmail.com>
2023-03-31 16:08:50 +0100

    Updates classes

--- templates/about.php
+++ templates/about.php
@@ -9,7 +9,7 @@
 					<?php include 'layout/side_navigation.php' ?>
 					<div class="container mx-auto">
             <div class="flex justify-center">
-              <h1 id="banner-heading" class="mt-56 font-Special 
+              <h1 id="banner-heading" class="mt-56 lg:mt-48 font-Special 
                 text-5xl lg:text-6xl text-biscuit drop-shadow-xl font-outline-2 animate-fade-in-text">
                   About me
               </h1>
@@ -19,10 +19,10 @@
 				<div class="bg-off-white full-bleed">
 					<div class="container mx-auto">
 						<div class="flex flex-row justify-around pt-4 lg:mb-8">
-							<div class="max-w-2/5">
+							<div class="max-w-[90%] md:max-w-2/5">
 								<?php include 'content/about-me.php' ?>
 							</div>	
-							<div class="max-w-2/5 mt-12">
+							<div class="hidden md:block max-w-2/5 mt-12">
 								<img src="../assets/images/stock.jpeg" alt="placeholder">
 							</div>
 						</div>
