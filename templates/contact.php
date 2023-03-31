89e7cb1e6ffef37f9b3cb0ff5f76882981d95933
Wilber30 <wilbersears@gmail.com>
2023-03-31 16:09:31 +0100

    Updates background colour

--- templates/contact.php
+++ templates/contact.php
@@ -3,7 +3,7 @@
 <body id="body" class="w-screen overflow-x-hidden">
   <div class="h-screen">
     <div id="cover" class="absolute h-full z-30 hidden bg-black/50 w-screen"></div>
-      <div class="flex flex-col justify-between h-screen bg-white">
+      <div class="flex flex-col justify-between h-screen bg-off-white">
         <div class="[&_.button-inner]:before:bg-black [&_.button-inner]:bg-black [&_.button-inner]:after:bg-black">
           <?php include 'layout/header.php' ?> 
 
