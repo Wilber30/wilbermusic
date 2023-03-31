5ea74f575f465f8d03851a6816fc5473242b806f
Wilber30 <wilbersears@gmail.com>
2023-03-31 16:09:16 +0100

    Adds image

--- templates/content/about-2.php
+++ templates/content/about-2.php
@@ -9,8 +9,13 @@
         <br><br>
     </p>
 
-    <p class="border-t-2 lg:border-grey p-5 text-center">
-		Thank you for stopping by! Please check out the video section of my site, for all my latest multi-instrumental compositions 
-		and solo guitar pieces. 
-	</p>
+	<div class="flex flex-col justify-center items-center border-t-2 border-grey">
+		<div class="flex justify-center md:hidden">
+			<img class="w-[90%] p-5" src="../assets/images/stock.jpeg" alt="placeholder">
+		</div>
+		<p class="pt-0 pb-5 py-5 md:p-5 text-center lg:w-[80%]">
+			Thank you for stopping by! Please check out the video section of my site, for all my latest multi-instrumental compositions 
+			and solo guitar pieces. 
+		</p>
+	</div>
 </div>
