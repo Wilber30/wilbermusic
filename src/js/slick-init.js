4056b2456e352030ec4908282d0554e589aef5a8
Wilber30 <wilbersears@gmail.com>
2023-06-27 13:54:45 +0100

    Adds javascript for carousel

---
+++ src/js/slick-init.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.slicker').slick({
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    fade: true,
+    });
+  });
+
+  
