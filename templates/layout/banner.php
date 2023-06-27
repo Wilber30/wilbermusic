59a810d63143ec207933e43a88dce6e8ac644450
Wilber30 <wilbersears@gmail.com>
2023-06-27 13:53:34 +0100

    Adds banner include

---
+++ templates/layout/banner.php
@@ -0,0 +1,20 @@
+<?php include 'templates/layout/header.php' ?>
+
+<?php include 'templates/layout/side_navigation.php' ?>
+<div class="container mx-auto">
+	<div class="flex justify-center">
+		<h1 id="banner-heading" class="mt-44 lg:mt-36 xl:mt-32 font-Special 
+			text-5xl lg:text-6xl text-biscuit drop-shadow-xl font-outline-2 animate-fade-in-text">
+				Wilber Sears
+		</h1>
+	</div>
+
+	<div class="container mx-auto absolute bottom-6">
+		<div class="flex justify-center">
+			<button class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white 
+				py-2 px-8 border border-gray-500 hover:border-transparent rounded">
+				<a href="index.php#latest-videos">Listen Now</a>
+			</button>
+		</div>
+	</div>
+</div>
