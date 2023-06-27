74bcd359351a190bbcb29843772afc3c7ede9b79
Wilber30 <wilbersears@gmail.com>
2023-06-27 13:55:17 +0100

    Adds stylesheet links for slick

--- templates/layout/head.php
+++ templates/layout/head.php
@@ -11,11 +11,19 @@
   <!-- Stylesheet links -->
   <link rel="stylesheet" href="/../dist/output.css">
   <link rel="stylesheet" href="/../node_modules/@fortawesome/fontawesome-free/css/all.css">
+  <link rel="stylesheet" href="/../node_modules/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="/../node_modules/slick-carousel/slick/slick-theme.css">
+
   <link rel="shortcut icon" type="image/png" href="/../assets/images/icons/favicon-32x32.png"/>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200;300;400;500;600;700&family=Roboto+Slab:wght@500;600;700&family=Shrikhand&display=swap" rel="stylesheet">
+
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
   <script src="/src/js/main.js" defer></script>
   <script type="text/javascript" src="/src/js/config.js"></script>
-  <script src="/src/js/video-player.js" defer></script>
+  <script src="/../node_modules/slick-carousel/slick/slick.js"></script>
+  <script src="/src/js/video-player.js"></script>
+  <script src="/src/js/slick-init.js"></script>
+
 </head> 
