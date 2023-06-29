bc41331376ae0552dfca78996f9eb33488a08a7c
Wilber30 <wilbersears@gmail.com>
2023-06-29 14:47:48 +0100

    Adds video component

---
+++ templates/components/video.php
@@ -0,0 +1,27 @@
+<div class="text-center py-10 bg-off-white">
+  <div class="container sm:mx-auto">
+    <div class="flex justify-center">
+      <div class="w-3/4 lg:w-1/3 border-dark-grey/50 border-b-4 border-solid">
+        <h1 id="latest-videos" class="font-Special mb-7 text-white font-outline-2 text-4xl lg:text-5xl">
+          Latest Videos
+        </h1>
+      </div>
+    </div>
+
+      <div class="mt-5">
+        <h2 class="p-3 text-2xl lg:text-3xl">Multi-Instrumental</h2>
+      </div>
+
+      <div>
+        <div class="youtube-container1 flex flex-col md:flex-row gap-6 items-center justify-center mt-4 mb-8"></div>
+    </div>
+
+    <div class="mt-5">
+        <h2 class="p-3 text-2xl lg:text-3xl">Guitar</h2>
+      </div>
+
+      <div>
+        <div class="youtube-container2 flex flex-col md:flex-row gap-6 items-center justify-center mt-4 mb-8"></div>
+    </div>
+  </div>
+</div>
