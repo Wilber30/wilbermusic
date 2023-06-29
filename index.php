3fd10435840696edef7bcb1126dd4aa274c309fd
Wilber30 <wilbersears@gmail.com>
2023-06-29 14:48:21 +0100

    Homepage refactor

--- index.php
+++ index.php
@@ -3,22 +3,17 @@
 <body id="body" class="w-screen overflow-x-hidden">
   <div class="full relative">
     <div id="cover" class="absolute h-full z-30 hidden bg-black/50 w-screen"></div>
-      <div class="relative">
-        <div class="h-screen bg-cover bg-bottom bg-hero-banner !w-screen">
-          <?php include 'templates/layout/banner.php' ?>
-        </div>
+    
+    <div class="relative">
+      <div class="h-screen bg-cover bg-bottom bg-hero-banner !w-screen">
+        <?php include 'templates/layout/banner.php' ?>
       </div>
-      <section>
-        <div class="text-center py-10 bg-off-white">
-          <div class="container sm:mx-auto">
-            <div class="md:border-b-4 md:border-dark-grey/50 md:border-solid"> 
-              <h1 id="latest-videos" class="font-Special mb-7 text-white font-outline-2 text-4xl lg:text-5xl">Latest Videos</h1>
-              <div class="youtube-container flex flex-col md:flex-row gap-6 items-center justify-center mt-4 mb-8"></div>
-            </div>
-            
-          </div>
-        </div>
-      </section>
-      <?php include 'templates/layout/footer.php' ?>
     </div>
+
+    <section>
+      <?php include 'templates/components/video.php' ?>
+    </section>
+
+    <?php include 'templates/layout/footer.php' ?>
+  </div>
 </body> 
