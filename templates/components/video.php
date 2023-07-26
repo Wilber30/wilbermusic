d1431874b4d72860fb6ecd37eb568eb012457aa1
Wilber30 <wilbersears@gmail.com>
2023-07-26 16:33:42 +0100

    Adds mark-up and styles

--- templates/components/video.php
+++ templates/components/video.php
@@ -1,27 +1,67 @@
-<div class="text-center py-10 bg-off-white">
-  <div class="container sm:mx-auto">
-    <div class="flex justify-center">
-      <div class="w-3/4 lg:w-1/3 border-dark-grey/50 border-b-4 border-solid">
-        <h1 id="latest-videos" class="font-Special mb-7 text-white font-outline-2 text-4xl lg:text-5xl">
+<div class="relative flex justify-center text-center pb-16 bg-antique-white">
+  <div class="hidden lg:block h-[70%] w-[40%] absolute mt-5 top-[-10%] left-[-110%] lg:left-[-45%] xl:left-[-35%] lg:rotate-[50deg]">
+    <img src="../assets/images/leafy-tree.png" alt="abstract tree">
+  </div>
+
+  <div class="hidden lg:block h-[70%] w-[40%] absolute mt-5 bottom-[-10%] right-[-60%] lg:right-[-30%]">
+    <img src="../assets/images/leafy-tree.png" alt="abstract tree">
+  </div>
+
+  <div class="hidden lg:block h-[70%] w-[40%] absolute mt-5 bottom-[-20%] left-[-110%] lg:left-[-55%] xl:left-[-45%] lg:rotate-[80deg]">
+    <img src="../assets/images/leafy-tree.png" alt="abstract tree">
+  </div>
+
+  <div class="video-container mx-auto">
+    <div class="title-wrapper flex justify-center">
+      <div class="w-full mt-6 mb-2 lg:mt-10 lg:mb-7 h-[15vh] flex flex-col items-center justify-center">
+        <h1 id="latest-videos" class="font-Star text-viridian font-extrabold text-[58px] lg:text-[68px]">
           Latest Videos
         </h1>
       </div>
     </div>
 
-    <div class="mt-5">
-      <h2 class="p-3 text-2xl lg:text-3xl">Multi-Instrumental</h2>
-    </div>
+    <div class="video-wrapper w-full flex justify-center flex-col items-center">
+      <div class="flex flex-row items-center">
+        <h2 class="font-Button p-3 text-xl lg:text-2xl">Multi-Instrumental</h2>
 
-    <div>
-        <div class="youtube-container1 flex flex-col md:flex-row gap-6 items-center justify-center mt-4 mb-8"></div>
-    </div>
+        <div class="flex flex-row gap-2">
+          <img class="h-[35px] w-[35px]" src="../assets/images/icons/xylophone.png" alt="xylophone">
 
-    <div class="mt-5">
-        <h2 class="p-3 text-2xl lg:text-3xl">Guitar</h2>
-    </div>
+          <img class="h-[35px] w-[35px]" src="../assets/images/icons/dulcimer.png" alt="dulcimer">
+        </div>
+      </div>
 
-    <div>
-      <div class="youtube-container2 flex flex-col md:flex-row gap-6 items-center justify-center mt-4 mb-8"></div>
+      <div class="swiffy-slider swiff-md-override" style="--swiffy-slider-nav-light:black;
+      --swiffy-slider-nav-zoom:1.25;">
+        <ul class="youtube-container1 slider-container mt-4 mb-8"></ul>
+
+        <button type="button" class="slider-nav slider-nav-prev 
+           after:!w-8 after:!h-8 sm:after:!w-12 sm:after:!h-12 sm:!left-[-70px]">
+        </button>
+
+        <button type="button" class="slider-nav slider-nav-next 
+            after:!w-8 after:!h-8 sm:after:!w-12 sm:after:!h-12 sm:!right-[-70px]">
+        </button>
+      </div>
+
+      <div class="flex flex-row items-center mt-5">
+        <h2 class="font-Button p-3 text-xl lg:text-2xl">Guitar</h2>
+
+        <img class="h-[35px] w-[35px]" src="../assets/images/icons/guitar.png" alt="acoustic guitar">
+      </div>
+
+      <div class="swiffy-slider swiff-md-override" style="--swiffy-slider-nav-light:black;
+      --swiffy-slider-nav-zoom:1.25;">
+        <ul class="youtube-container2 slider-container mt-4 mb-8"></ul>
+
+        <button type="button" class="slider-nav slider-nav-prev 
+           after:!w-8 after:!h-8 sm:after:!w-12 sm:after:!h-12 sm:!left-[-70px]">
+        </button>
+
+        <button type="button" class="slider-nav slider-nav-next 
+            after:!w-8 after:!h-8 sm:after:!w-12 sm:after:!h-12 sm:!right-[-70px]">
+        </button>
+      </div>
     </div>
   </div>
 </div>
