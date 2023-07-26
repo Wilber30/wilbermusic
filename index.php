1f90748dbbb87658209d55da44b3532666aee626
Wilber30 <wilbersears@gmail.com>
2023-07-26 16:35:13 +0100

    Adjusts tw classes

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include 'templates/layout/head.php' ?>
 
-<body id="body" class="w-screen overflow-x-hidden">
-  <div class="full relative">
-    <div id="cover" class="absolute h-full z-30 hidden bg-black/50 w-screen"></div>
+<body id="body" class="w-screen">
+  <div class="full overflow-x-hidden overflow-y-hidden">
+    <div id="cover" class="absolute h-screen z-30 hidden bg-black/50 w-screen"></div>
     
     <div class="relative">
       <div class="h-screen bg-cover bg-bottom bg-hero-banner !w-screen">
@@ -10,9 +10,8 @@
       </div>
     </div>
 
-    <section>
       <?php include 'templates/components/video.php' ?>
-    </section>
+   
 
     <?php include 'templates/layout/footer.php' ?>
   </div>
