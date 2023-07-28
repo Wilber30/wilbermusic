ff9708524726d812069fc42b6b34e3cef63e97b1
Wilber30 <wilbersears@gmail.com>
2023-07-28 21:37:21 +0100

    Updates  and icons

--- templates/components/video.php
+++ templates/components/video.php
@@ -1,69 +1,76 @@
-<div class="relative flex justify-center text-center pb-16 bg-antique-white">
+<div id="video-section" class="relative flex justify-center text-center bg-dark-slate-grey">
   <div id="video-cover" class="absolute h-full z-30 hidden bg-black/50 w-screen"></div>
 
-  <div class="hidden lg:block h-[70%] w-[40%] absolute mt-5 top-[-10%] left-[-110%] lg:left-[-45%] xl:left-[-35%] lg:rotate-[50deg]">
-    <img src="../assets/images/leafy-tree.png" alt="abstract tree">
+  <div class="hidden lg:block h-[70%] w-[40%] absolute mt-5 top-[-5%] left-[-110%] lg:left-[-45%] xl:left-[-35%] lg:rotate-[50deg]">
+    <img src="../assets/images/brown-leafy-tree.png" alt="abstract tree">
   </div>
 
   <div class="hidden lg:block h-[70%] w-[40%] absolute mt-5 bottom-[-10%] right-[-60%] lg:right-[-30%]">
-    <img src="../assets/images/leafy-tree.png" alt="abstract tree">
+    <img src="../assets/images/brown-leafy-tree.png" alt="abstract tree">
   </div>
 
   <div class="hidden lg:block h-[70%] w-[40%] absolute mt-5 bottom-[-20%] left-[-110%] lg:left-[-55%] xl:left-[-45%] lg:rotate-[80deg]">
-    <img src="../assets/images/leafy-tree.png" alt="abstract tree">
+    <img src="../assets/images/brown-leafy-tree.png" alt="abstract tree">
   </div>
 
   <div class="video-container mx-auto">
     <div class="title-wrapper flex justify-center">
       <div class="w-full mt-6 mb-2 lg:mt-10 lg:mb-7 h-[15vh] flex flex-col items-center justify-center">
-        <h1 id="latest-videos" class="font-Star text-viridian font-extrabold text-[38px] md:text-[58px] lg:text-[68px]">
+        <h1 id="latest-videos" class="font-Star text-biscuit font-extrabold text-[38px] md:text-[58px] lg:text-[68px]">
           Latest Videos
         </h1>
       </div>
     </div>
 
     <div class="video-wrapper w-full flex justify-center flex-col items-center">
-      <div class="flex flex-row items-center">
+      <div class="flex flex-row items-center text-biscuit">
         <h2 class="font-Button p-3 text-xl lg:text-2xl">Multi-Instrumental</h2>
 
         <div class="flex flex-row gap-2">
-          <img class="h-[35px] w-[35px]" src="../assets/images/icons/xylophone.png" alt="xylophone">
+          <img class="h-[35px] w-[35px]" src="../assets/images/icons/brown-xylophone.png" alt="xylophone">
 
-          <img class="h-[35px] w-[35px]" src="../assets/images/icons/dulcimer.png" alt="dulcimer">
+          <img class="h-[35px] w-[35px]" src="../assets/images/icons/browny-dulcimer.png" alt="dulcimer">
         </div>
       </div>
 
       <div class="swiffy-slider swiff-md-override" style="--swiffy-slider-nav-light:black;
       --swiffy-slider-nav-zoom:1.25;">
-        <ul class="youtube-container1 slider-container mt-4 mb-8"></ul>
+        <ul class="youtube-container1 slider-container !mt-2 !mb-8"></ul>
 
-        <button type="button" class="slider-nav slider-nav-prev 
+        <button type="button" class="slider-nav after:!bg-white slider-nav-prev 
            after:!w-8 after:!h-8 sm:after:!w-12 sm:after:!h-12 sm:!left-[-70px]">
         </button>
 
-        <button type="button" class="slider-nav slider-nav-next 
+        <button type="button" class="slider-nav after:!bg-white slider-nav-next 
             after:!w-8 after:!h-8 sm:after:!w-12 sm:after:!h-12 sm:!right-[-70px]">
         </button>
       </div>
 
-      <div class="flex flex-row items-center mt-5">
+      <div class="flex flex-row items-center mt-5 text-biscuit">
         <h2 class="font-Button p-3 text-xl lg:text-2xl">Guitar</h2>
 
-        <img class="h-[35px] w-[35px]" src="../assets/images/icons/guitar.png" alt="acoustic guitar">
+        <img class="h-[35px] w-[35px]" src="../assets/images/icons/brown-guitar.png" alt="acoustic guitar">
       </div>
 
       <div class="swiffy-slider swiff-md-override" style="--swiffy-slider-nav-light:black;
       --swiffy-slider-nav-zoom:1.25;">
-        <ul class="youtube-container2 slider-container mt-4 mb-8"></ul>
+        <ul class="youtube-container2 slider-container !mt-2 !mb-8"></ul>
 
-        <button type="button" class="slider-nav slider-nav-prev 
+        <button type="button" class="slider-nav after:!bg-white slider-nav-prev 
            after:!w-8 after:!h-8 sm:after:!w-12 sm:after:!h-12 sm:!left-[-70px]">
         </button>
 
-        <button type="button" class="slider-nav slider-nav-next 
+        <button type="button" class="slider-nav after:!bg-white slider-nav-next 
             after:!w-8 after:!h-8 sm:after:!w-12 sm:after:!h-12 sm:!right-[-70px]">
         </button>
       </div>
+    </div>
+    <div class="flex justify-center flex-col mb-10 text-xl text-biscuit">
+      <a href="#top">
+        <i class="fa-solid fa-chevron-up"></i>
+
+        <p class="font-Button">Back to the Top</p>
+      </a>
     </div>
   </div>
 </div>
