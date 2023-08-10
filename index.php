99697db1612b1f24afa80bf3fd7015fb836f8260
Wilber30 <wilbersears@gmail.com>
2023-08-10 17:57:58 +0100

    Updates classes and mark-up

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'templates/layout/head.php' ?>
 
-<body id="body" class="w-screen">
-  <div class="full overflow-x-hidden overflow-y-hidden">
+<body id="body" class="w-screen overflow-x-hidden">
+  <div class="full overflow-x-hidden">
     <div id="coverMobile" class="absolute h-screen z-30 hidden bg-black/50 w-screen"></div>
     <div id="cover" class="absolute h-screen z-30 hidden bg-black/50 w-screen"></div>
     
@@ -10,10 +10,9 @@
         <?php include 'templates/layout/banner.php' ?>
       </div>
     </div>
-
-      <?php include 'templates/components/video.php' ?>
-   
-
+    <?php include 'templates/components/video.php' ?>
+  
     <?php include 'templates/layout/footer.php' ?>
   </div>
 </body> 
+</html>
