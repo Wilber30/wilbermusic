6ac94fe6908f0b1952e413bfca9232135560431c
Wilber30 <wilbersears@gmail.com>
2023-08-10 17:58:45 +0100

    Adds a href

--- templates/content/about-2.php
+++ templates/content/about-2.php
@@ -14,7 +14,7 @@
 			<img class="w-[90%] p-5" src="../assets/images/circular_sears.png" alt="placeholder">
 		</div>
 		<p class="pt-0 pb-5 py-5 md:p-5 text-center lg:w-[80%]">
-			Thank you for stopping by! Please check out the <a href="../#video-main">video section</a> of my site, for all my latest multi-instrumental compositions 
+			Thank you for stopping by! Please check out the <a href="../index.php#video-section">video section</a> of my site, for all my latest multi-instrumental compositions 
 			and solo guitar pieces. 
 		</p>
 	</div>
